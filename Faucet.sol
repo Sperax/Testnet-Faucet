e5472fe0c0df4cb4ce6d5430ce5dd78a8d9fc7a5
Mihir <mwadekar2000@gmail.com>
2021-01-18 13:53:57 -0800

    Added comments and indentation to the soilidity contract

--- Faucet.sol
+++ Faucet.sol
@@ -1,46 +1,43 @@
+// This contract creates a testnet faucet on the rinkeby testnet and will 
+// be later migrated to the Sperax Testnet
+
+// References 
+// https://ethereum.stackexchange.com/questions/19314/how-to-transfer-from-one-ether-wallet-to-another-using-smartcontract
+
 pragma solidity ^0.6.0;
 
 contract Faucet {
-    
-    mapping (address => uint256) public access_time; 
-    
-    constructor() public {
-        
+  // Mapping storing the users last access time
+  mapping (address => uint256) public access_time; 
+  
+  constructor() public {}
+
+  // To handle receiving payments from the wallet
+  receive() external payable {}
+  
+  // Gives out the eth to the requester
+  function get_eth (address payable requester) public returns (bool) {
+    // Make sure that the balance of the address is more than 1
+    require(address(this).balance > 10000000000000000, "Issue");
+
+    if (access_time[requester] == 0) {
+      // Update access time for requester
+      access_time[requester] = now;
+      // Transfer ETH from contract to requester
+      requester.transfer(10000000000000000);
+      return true;
+
+    } else {
+      if (now - access_time[requester] < 86400) {
+        // If requester requests within 24 hours deny the request
+        return false;
+      } else {
+        // Update access time for requester
+        access_time[requester] = now;
+        // Transfer ETH to requester
+        requester.transfer(10000000000000000);
+        return true;
+      }
     }
-    
-    // event Received(address, uint);
-    receive() external payable {
-        // emit Received(msg.sender, msg.value);
-    }
-    
-    function get_eth (address payable requester) public returns (bool) {
-    	// Make sure that the balance of the address is more than 1
-    	require(address(this).balance > 10000000000000000, "Issue");
-
-        if (access_time[requester] == 0) {
-     		// Update access time for requester
-    		access_time[requester] = now;
-
-	    	// Transfer ETH from contract to requester
-	    	requester.transfer(10000000000000000);
-
-    		return true;
-
-        } else {
-        	if (now - access_time[requester] < 86400) {
-        		return false;
-        	} else {
-	     		// Update access time for requester
-        		access_time[requester] = now;
-		    	
-		    	// Transfer ETH to requester
-		    	requester.transfer(10000000000000000);
-
-    	        return true;
-        	}
-        }
-    }
-    
+  }
 }
-
-//https://ethereum.stackexchange.com/questions/19314/how-to-transfer-from-one-ether-wallet-to-another-using-smartcontract
